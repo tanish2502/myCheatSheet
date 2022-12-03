995a3e03be0f1d67eb58abab39e5ffeffd9caccf
tanish2502 <100233384+tanish2502@users.noreply.github.com>
2022-12-03 23:52:28 +0530

    C++ ModularProg

---
+++ C++/C++ Code/C++Session3/date.hpp
@@ -0,0 +1,37 @@
+#ifndef _DATE_H
+#define _DATE_H
+
+#define SUCCESS 0
+#define TRUE 0
+#define FALSE 1
+
+class Date
+{
+    private:
+        int day;
+        int month;
+        int year;
+
+        bool static isDateValid(int day, int month, int year);
+
+    public:
+        Date(int initDay, int initMonth, int initYear){
+            day = initDay;
+            month = initMonth;
+            year = initYear;
+        };
+
+        int getDay() const;
+        int setDay(int newDay);
+
+        int getMonth() const;
+        int setMonth(int newMonth);
+
+        int getYear() const;
+        int setYear(int newYear);
+
+        void showDate() const;
+
+};
+
+#endif /*_DATE_H*/
